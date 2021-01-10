ed12053a9306b01aea45496199ed706feb9a8797
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-01-10 19:28:36 -0300

    Add files via upload

---
+++ Lesson 06 - Assignments/script.js
@@ -0,0 +1,18 @@
+// =    x=y    x=y
+// +=   x += y  x = x+y
+// -=   x -= y  x = x-y
+// *=   x *= y  x = x*y
+// /=   x /= y  x = x/y
+// %=   x %= y  x = x%y
+
+
+var a = 30;
+
+// a += 5; 
+/*É IGUAL*/
+a = a + 5;
+
+console.log(a);
+
+
+
