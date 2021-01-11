bf1914a96ebf920ef8631d9d9e4bc370ef22ba47
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-01-11 16:44:25 -0300

    Add files via upload

---
+++ Lesson 07 - Comparative Operators/script.js
@@ -0,0 +1,16 @@
+// == igual a
+// === valor igual e igual
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
+
+
+var a = 7;
+var b="5";
+
+console.log(a<=b);
+
+
