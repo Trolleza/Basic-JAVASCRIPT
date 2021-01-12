755389a6ecf4847433a8c0d3cddbcf031f9a3776
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-01-12 15:48:03 -0300

    Add files via upload

---
+++ Lesson 08 - Logical Operators/script.js
@@ -0,0 +1,23 @@
+// && e
+// || ou
+
+
+
+
+//var a = false;
+//var b = false;
+
+//var c = a && b;
+
+//console.log(c);
+
+var idade = 9;
+
+var menor10 = idade <= 10;
+var maior65 = idade >= 65;
+var gratuidade = menor10 || maior65;
+
+console.log("Idade", idade);
+console.log("Maior que 65", maior65);
+console.log("Menor que 10", menor10);
+console.log("Tem Benefício?", gratuidade);
