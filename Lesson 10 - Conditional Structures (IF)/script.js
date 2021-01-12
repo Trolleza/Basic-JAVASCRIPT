530c222ed176f2c8e6c710a1aea73f72e91b0981
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-01-12 17:09:21 -0300

    Add files via upload

---
+++ Lesson 10 - Conditional Structures (IF)/script.js
@@ -0,0 +1,12 @@
+var idade = 18;
+
+if (idade < 18 || idade > 70){
+    console.log("pode");
+    console.log("Qual o seu pedido?");}
+    
+else{
+    console.log("não pode");
+    console.log("Volte Futuramente!");}
+    
+
+
