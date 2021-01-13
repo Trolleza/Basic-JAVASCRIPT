fe248499774dc751b287b24b984daf8262d1fe12
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-01-13 16:25:12 -0300

    Add files via upload

---
+++ Lesson 13 - FOR and WHILE Repetition/script.js
@@ -0,0 +1,13 @@
+var numero = Math.random() * 100;
+
+
+while(numero  < 90){
+    console.log(numero);
+    numero = Math.random() * 100;}
+
+console.log(numero)
+console.log("Acabou");
+
+
+//for(var i = 10; i < numero; i++){
+    //console.log("Executando o for, pela " + i + " Vez.");}
