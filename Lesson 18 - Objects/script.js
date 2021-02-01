b15e3b74ced1029a75ff7df95d259a42fb2c9971
Graziele Dantas <74679475+Trolleza@users.noreply.github.com>
2021-02-01 19:06:14 -0300

    Add files via upload

---
+++ Lesson 18 - Objects/script.js
@@ -0,0 +1,13 @@
+var aluno={
+    nome:"Igor", 
+    notas:[7.5, 5.0]};
+
+var NomeDeUmaNovaPropriedade = "LastName";
+
+aluno.matricula = 12345;
+
+aluno[NomeDeUmaNovaPropriedade] = "Oliveira";
+
+console.log(aluno);
+
+
